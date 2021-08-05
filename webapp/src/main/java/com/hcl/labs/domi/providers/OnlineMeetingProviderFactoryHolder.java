f561e233088f46bdef14cf4daa0851610c47b5a1
Devin S. Olson <devin.olson@hcl.com>
2021-08-05 19:32:51 -0400

    Changed references from (crufty) DOMIConstants values to DOMIProvider.instance properties

--- webapp/src/main/java/com/hcl/labs/domi/providers/OnlineMeetingProviderFactoryHolder.java
+++ webapp/src/main/java/com/hcl/labs/domi/providers/OnlineMeetingProviderFactoryHolder.java
@@ -19,11 +19,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
-import java.security.SecureRandom;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
-import javax.security.sasl.AuthenticationException;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -35,9 +32,9 @@ import com.github.mustachejava.MustacheFactory;
 import com.hcl.labs.domi.RefreshTokenHandler;
 import com.hcl.labs.domi.RevokeTokenHandler;
 import com.hcl.labs.domi.tools.DOMIConstants;
+import com.hcl.labs.domi.tools.DOMIProvider;
 import com.hcl.labs.domi.tools.DOMIUtils;
 
-import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerResponse;
@@ -45,7 +42,6 @@ import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
 import io.vertx.ext.auth.oauth2.OAuth2Options;
 import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.Session;
 import io.vertx.ext.web.handler.OAuth2AuthHandler;
 
 /**
@@ -173,44 +169,12 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
     }
 
     // Zoom doesn't follow IETF standards for revoke, so we need overridden classes
-    final OAuth2Auth oAuthProvider = DOMIConstants.ZOOM_REVOKE_ROUTE.equals(params.revokeRoute)
+    final OAuth2Auth oAuthProvider = DOMIProvider.ZOOM.REVOKE_ROUTE.equals(params.revokeRoute)
         ? new ZoomOAuth2AuthProviderImpl(params.vertx, options)
         : OAuth2Auth.create(params.vertx, options);
 
     final OAuth2AuthHandler oAuthHandler = OAuth2AuthHandler.create(params.vertx, oAuthProvider,
         getHostName() + params.callbackRoute);
-
-    // TODO Spanky - add session handler to reject invalid URLS and then immediately
-    // purge any session context information.
-
-    // ChainAuthHandler chain = ChainAuthHandler.any()
-    // .add(oAuthHandler)
-    // .add(ChainAuthHandler.all());
-    //
-    // params.router.route(params.path + "*").handler(chain);
-
-    // ChainAuthHandler chain = ChainAuthHandler.any()
-    // .add(oAuthHandler)
-    // .add(ChainAuthHandler.all());
-
-    /*
-     
-     ChainAuthHandler chain =
-      ChainAuthHandler.any()
-    .add(authNHandlerA)
-    .add(ChainAuthHandler.all()
-      .add(authNHandlerB)
-      .add(authNHandlerC));
-     
-     
-    // secure your route
-    router.route("/secure/resource").handler(chain);
-    // your app
-    router.route("/secure/resource").handler(ctx -> {
-      // do something...
-    });
-     
-     */
 
     if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
       OnlineMeetingProviderFactoryHolder.LOGGER.info("Enabled OAuth callback for {} at {}",
@@ -228,80 +192,7 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
     // OAuth handler
     params.router.route(indexRoot).handler(ctx -> oAuthTokenHandler(ctx,
         getHostName() + indexRoot));
-
-    // BEGIN SPANKY TESTING
-    /*
-    SessionStore sessionStore = LocalSessionStore.create(params.vertx);
-    SessionHandler sessionHandler = SessionHandler.create(sessionStore);
-    sessionHandler.setCookieSameSite(CookieSameSite.STRICT);
-    params.router.route(params.path + "*").handler(sessionHandler);
-    if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
-      OnlineMeetingProviderFactoryHolder.LOGGER.info("Enabled cookie checks for {} on paths {}",
-          getProviderName(), getHostName() + params.path + "*");
-    }
-    
-    OnlineMeetingProviderFactoryHolder.LOGGER.info("(params.path + \"*\": " + params.path + "*");
-    OnlineMeetingProviderFactoryHolder.LOGGER.info("indexRoot: " + indexRoot);
-    
-    params.router.route("/*").handler(ctx -> OnlineMeetingProviderFactoryHolder.sessionCookieHandler(ctx));
-    */
-    // END SPANKY TESTING
-
     return oAuthProvider;
-  }
-
-  private static void sessionCookieHandler(final RoutingContext ctx) {
-
-    SecureRandom secureRandom = new SecureRandom();
-    byte[] sourceValues = new byte[128];
-    secureRandom.nextBytes(sourceValues);
-
-    int randomInt = secureRandom.nextInt();
-    String cookieName = "vtx" + randomInt;
-
-    String sourceCookie = "";
-    for (byte b : sourceValues) {
-      sourceCookie += b;
-    }
-
-    try {
-
-      Session session = ctx.session();
-      if (null != session) {
-      }
-
-      // Put some data from the session
-      session.put(cookieName, sourceCookie);
-      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
-        OnlineMeetingProviderFactoryHolder.LOGGER.info("Put value '" + sourceCookie + "' into cookie '" + cookieName + "'");
-      }
-
-      // Retrieve some data from a session
-      String targetCookie = session.get(cookieName);
-      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
-        OnlineMeetingProviderFactoryHolder.LOGGER.info("Retrieved value '" + targetCookie + "' from cookie '" + cookieName + "'");
-      }
-
-      // Remove some data from a session
-      Object obj = session.remove(cookieName);
-      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
-        OnlineMeetingProviderFactoryHolder.LOGGER.info("Removed cookie '" + cookieName + "' with value '" + obj.toString() + "'");
-      }
-
-      String foo = session.get(cookieName);
-      foo = (null != foo) ? "'" + foo + "'" : "(null)";
-      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
-        OnlineMeetingProviderFactoryHolder.LOGGER.info("Retrieved value " + foo + " from cookie '" + cookieName + "'");
-      }
-
-      if (!sourceCookie.equals(targetCookie)) {
-        throw new AuthenticationException("Cookies do not match!");
-      }
-
-    } catch (Exception e) {
-      OnlineMeetingProviderFactoryHolder.LOGGER.error("SecureRandom Cookie Failure", DOMIConstants.TOKEN_PAGE, e);
-      ctx.fail(500);
-    }
   }
 
   /**
@@ -320,24 +211,6 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
         .putHeader(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, must-revalidate")
         .putHeader("Pragma", "no-cache")
         .putHeader(HttpHeaders.EXPIRES, "0");
-
-    // TODO Spanky Testing
-    MultiMap mm = ctx.queryParams();
-    if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
-      OnlineMeetingProviderFactoryHolder.LOGGER.info("normalizedPath(): " + ctx.normalizedPath());
-      OnlineMeetingProviderFactoryHolder.LOGGER.info("queryParams(): " + mm.toString());
-      if (mm.contains("state")) {
-        OnlineMeetingProviderFactoryHolder.LOGGER.info("State Argument Exists");
-        String state = mm.get("state");
-        OnlineMeetingProviderFactoryHolder.LOGGER.info("state: " + state);
-        mm.remove("state");
-
-        state = mm.get("state");
-        OnlineMeetingProviderFactoryHolder.LOGGER.info("state: " + state);
-
-      }
-
-    }
 
     final String pageResource = DOMIConstants.TOKEN_PAGE;
     try (InputStream inputStream = DOMIUtils.class.getResourceAsStream(pageResource);
