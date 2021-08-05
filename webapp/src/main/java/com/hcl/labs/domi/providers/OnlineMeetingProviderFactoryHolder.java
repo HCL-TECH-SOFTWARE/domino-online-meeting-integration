fafb0cfdcbd8e5e817a72cdf930245e510d90b81
Devin S. Olson <devin.olson@hcl.com>
2021-08-05 17:54:34 -0400

    UNSTABLE - Dev / Testing various solution paths for Open Redirect state issue.

--- webapp/src/main/java/com/hcl/labs/domi/providers/OnlineMeetingProviderFactoryHolder.java
+++ webapp/src/main/java/com/hcl/labs/domi/providers/OnlineMeetingProviderFactoryHolder.java
@@ -19,11 +19,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import javax.security.sasl.AuthenticationException;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.github.mustachejava.DefaultMustacheFactory;
 import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
@@ -31,6 +36,8 @@ import com.hcl.labs.domi.RefreshTokenHandler;
 import com.hcl.labs.domi.RevokeTokenHandler;
 import com.hcl.labs.domi.tools.DOMIConstants;
 import com.hcl.labs.domi.tools.DOMIUtils;
+
+import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.http.HttpServerResponse;
@@ -38,6 +45,7 @@ import io.vertx.ext.auth.oauth2.OAuth2Auth;
 import io.vertx.ext.auth.oauth2.OAuth2FlowType;
 import io.vertx.ext.auth.oauth2.OAuth2Options;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.Session;
 import io.vertx.ext.web.handler.OAuth2AuthHandler;
 
 /**
@@ -46,8 +54,7 @@ import io.vertx.ext.web.handler.OAuth2AuthHandler;
  */
 public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProviderFactory {
 
-  private static final Logger LOGGER =
-      LoggerFactory.getLogger(OnlineMeetingProviderFactoryHolder.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(OnlineMeetingProviderFactoryHolder.class);
   private final String clientId; // Client ID to use when connecting to the OAuth provider
   private final String clientSecret; // Client secret to use when connecting to the OAuth provider
   private final String providerName; // Provider name, used in messages and web page
@@ -56,10 +63,10 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
   /**
    * Constructor
    * 
-   * @param clientId of OAuth application for this provider
+   * @param clientId     of OAuth application for this provider
    * @param clientSecret of OAuth application for this provider
    * @param providerName for online meeting provider
-   * @param hostName for this server, to use in redirect URLs etc
+   * @param hostName     for this server, to use in redirect URLs etc
    */
   public OnlineMeetingProviderFactoryHolder(final String clientId, final String clientSecret,
       final String providerName, final String hostName) {
@@ -123,8 +130,7 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
 
       // Add revoke route, if needed
       if (StringUtils.isNotEmpty(params.revokeRoute) && StringUtils.isNotEmpty(params.revocationUrl)) {
-        final RevokeTokenHandler revokeHandler =
-            new RevokeTokenHandler(oauthProvider, getProviderName());
+        final RevokeTokenHandler revokeHandler = new RevokeTokenHandler(oauthProvider, getProviderName());
         params.router.route(params.revokeRoute).handler(revokeHandler);
         if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
           OnlineMeetingProviderFactoryHolder.LOGGER.info(
@@ -133,8 +139,7 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
         }
       }
 
-      final RefreshTokenHandler refreshHandler =
-          new RefreshTokenHandler(oauthProvider, getProviderName());
+      final RefreshTokenHandler refreshHandler = new RefreshTokenHandler(oauthProvider, getProviderName());
       params.router.route(HttpMethod.GET, params.refreshRoute).handler(refreshHandler);
       if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
         OnlineMeetingProviderFactoryHolder.LOGGER.info("Enabled OAuth token refresh for {} at {}",
@@ -144,9 +149,11 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
   }
 
   /**
-   * Create all endpoints for OAuth dance and display of initial access and refresh tokens
+   * Create all end points for OAuth dance and display of initial access and
+   * refresh tokens
    * 
-   * @param params OnlineMeetingProviderParameters object holding all required settings
+   * @param params OnlineMeetingProviderParameters object holding all required
+   *               settings
    * @return OAuth2Auth authentication provider
    */
   private OAuth2Auth createOAuthRoutesAndHandler(final OnlineMeetingProviderParameters params) {
@@ -165,14 +172,46 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
       options.setRevocationPath(params.revocationUrl);
     }
 
-    // Zoom doesn't follow IETF standards for revoke, so we need overrided classes
+    // Zoom doesn't follow IETF standards for revoke, so we need overridden classes
     final OAuth2Auth oAuthProvider = DOMIConstants.ZOOM_REVOKE_ROUTE.equals(params.revokeRoute)
         ? new ZoomOAuth2AuthProviderImpl(params.vertx, options)
         : OAuth2Auth.create(params.vertx, options);
 
-    final OAuth2AuthHandler oAuthHandler =
-        OAuth2AuthHandler.create(params.vertx, oAuthProvider,
-            getHostName() + params.callbackRoute);
+    final OAuth2AuthHandler oAuthHandler = OAuth2AuthHandler.create(params.vertx, oAuthProvider,
+        getHostName() + params.callbackRoute);
+
+    // TODO Spanky - add session handler to reject invalid URLS and then immediately
+    // purge any session context information.
+
+    // ChainAuthHandler chain = ChainAuthHandler.any()
+    // .add(oAuthHandler)
+    // .add(ChainAuthHandler.all());
+    //
+    // params.router.route(params.path + "*").handler(chain);
+
+    // ChainAuthHandler chain = ChainAuthHandler.any()
+    // .add(oAuthHandler)
+    // .add(ChainAuthHandler.all());
+
+    /*
+     
+     ChainAuthHandler chain =
+      ChainAuthHandler.any()
+    .add(authNHandlerA)
+    .add(ChainAuthHandler.all()
+      .add(authNHandlerB)
+      .add(authNHandlerC));
+     
+     
+    // secure your route
+    router.route("/secure/resource").handler(chain);
+    // your app
+    router.route("/secure/resource").handler(ctx -> {
+      // do something...
+    });
+     
+     */
+
     if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
       OnlineMeetingProviderFactoryHolder.LOGGER.info("Enabled OAuth callback for {} at {}",
           getProviderName(), getHostName() + params.callbackRoute);
@@ -186,17 +225,93 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
           getProviderName(), getHostName() + params.path + "*");
     }
 
-    params.router.route(indexRoot)
-        .handler(ctx -> oAuthTokenHandler(ctx, getHostName() + indexRoot));
+    // OAuth handler
+    params.router.route(indexRoot).handler(ctx -> oAuthTokenHandler(ctx,
+        getHostName() + indexRoot));
+
+    // BEGIN SPANKY TESTING
+    /*
+    SessionStore sessionStore = LocalSessionStore.create(params.vertx);
+    SessionHandler sessionHandler = SessionHandler.create(sessionStore);
+    sessionHandler.setCookieSameSite(CookieSameSite.STRICT);
+    params.router.route(params.path + "*").handler(sessionHandler);
+    if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
+      OnlineMeetingProviderFactoryHolder.LOGGER.info("Enabled cookie checks for {} on paths {}",
+          getProviderName(), getHostName() + params.path + "*");
+    }
+    
+    OnlineMeetingProviderFactoryHolder.LOGGER.info("(params.path + \"*\": " + params.path + "*");
+    OnlineMeetingProviderFactoryHolder.LOGGER.info("indexRoot: " + indexRoot);
+    
+    params.router.route("/*").handler(ctx -> OnlineMeetingProviderFactoryHolder.sessionCookieHandler(ctx));
+    */
+    // END SPANKY TESTING
+
     return oAuthProvider;
   }
 
+  private static void sessionCookieHandler(final RoutingContext ctx) {
+
+    SecureRandom secureRandom = new SecureRandom();
+    byte[] sourceValues = new byte[128];
+    secureRandom.nextBytes(sourceValues);
+
+    int randomInt = secureRandom.nextInt();
+    String cookieName = "vtx" + randomInt;
+
+    String sourceCookie = "";
+    for (byte b : sourceValues) {
+      sourceCookie += b;
+    }
+
+    try {
+
+      Session session = ctx.session();
+      if (null != session) {
+      }
+
+      // Put some data from the session
+      session.put(cookieName, sourceCookie);
+      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
+        OnlineMeetingProviderFactoryHolder.LOGGER.info("Put value '" + sourceCookie + "' into cookie '" + cookieName + "'");
+      }
+
+      // Retrieve some data from a session
+      String targetCookie = session.get(cookieName);
+      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
+        OnlineMeetingProviderFactoryHolder.LOGGER.info("Retrieved value '" + targetCookie + "' from cookie '" + cookieName + "'");
+      }
+
+      // Remove some data from a session
+      Object obj = session.remove(cookieName);
+      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
+        OnlineMeetingProviderFactoryHolder.LOGGER.info("Removed cookie '" + cookieName + "' with value '" + obj.toString() + "'");
+      }
+
+      String foo = session.get(cookieName);
+      foo = (null != foo) ? "'" + foo + "'" : "(null)";
+      if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
+        OnlineMeetingProviderFactoryHolder.LOGGER.info("Retrieved value " + foo + " from cookie '" + cookieName + "'");
+      }
+
+      if (!sourceCookie.equals(targetCookie)) {
+        throw new AuthenticationException("Cookies do not match!");
+      }
+
+    } catch (Exception e) {
+      OnlineMeetingProviderFactoryHolder.LOGGER.error("SecureRandom Cookie Failure", DOMIConstants.TOKEN_PAGE, e);
+      ctx.fail(500);
+    }
+  }
+
   /**
-   * Handler to generate page with access and refresh tokens for this online meeting provider
+   * Handler to generate page with access and refresh tokens for this online
+   * meeting provider
    * 
-   * @param ctx current RoutingContext
+   * @param ctx      current RoutingContext
    * @param location to override URL in location bar
    */
+  @SuppressWarnings("resource")
   private void oAuthTokenHandler(final RoutingContext ctx, final String location) {
     final HttpServerResponse response = ctx.response();
     DOMIUtils.incrementRequestCounter(DOMIConstants.METRIC_TOTAL_HTTP, getProviderName(),
@@ -206,10 +321,27 @@ public class OnlineMeetingProviderFactoryHolder implements OnlineMeetingProvider
         .putHeader("Pragma", "no-cache")
         .putHeader(HttpHeaders.EXPIRES, "0");
 
+    // TODO Spanky Testing
+    MultiMap mm = ctx.queryParams();
+    if (OnlineMeetingProviderFactoryHolder.LOGGER.isInfoEnabled()) {
+      OnlineMeetingProviderFactoryHolder.LOGGER.info("normalizedPath(): " + ctx.normalizedPath());
+      OnlineMeetingProviderFactoryHolder.LOGGER.info("queryParams(): " + mm.toString());
+      if (mm.contains("state")) {
+        OnlineMeetingProviderFactoryHolder.LOGGER.info("State Argument Exists");
+        String state = mm.get("state");
+        OnlineMeetingProviderFactoryHolder.LOGGER.info("state: " + state);
+        mm.remove("state");
+
+        state = mm.get("state");
+        OnlineMeetingProviderFactoryHolder.LOGGER.info("state: " + state);
+
+      }
+
+    }
+
     final String pageResource = DOMIConstants.TOKEN_PAGE;
     try (InputStream inputStream = DOMIUtils.class.getResourceAsStream(pageResource);
-        InputStreamReader reader =
-            new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
+        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8)) {
       final MustacheFactory mustFact = new DefaultMustacheFactory();
       final Map<String, String> details = new ConcurrentHashMap<>();
       details.put("OAUTH_TYPE", getProviderName());
