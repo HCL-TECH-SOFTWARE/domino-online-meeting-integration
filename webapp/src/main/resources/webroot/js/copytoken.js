4f4200d3c38e5be91f4df95eca9c3b4ca9cab65d
Devin S. Olson <devin.olson@hcl.com>
2022-07-05 17:45:35 -0400

    replaced jquery calls with explicit event listener calls.

--- webapp/src/main/resources/webroot/js/copytoken.js
+++ webapp/src/main/resources/webroot/js/copytoken.js
@@ -4,7 +4,7 @@
   * Licensed under Apache License, Version 2.0, January 2004 (http://www.apache.org/licenses/)
   */
 
-  function pushdatafromlocation() {
+   function pushdatafromlocation() {
     var div = document.getElementById("divlocation");
     var content = div.innerText;
     window.history.pushState('data', '', content);
@@ -13,21 +13,38 @@
 
   function addcopytoken() {
     var btn = document.getElementById('btncopy');
+
     btn.onclick = function (e) {
-        var ta = document.createElement('textarea');
-        ta.value = $(this).next()[0].innerText;
-        ta.style.position = 'absolute';
-        ta.style.left = '-3000px';
-        document.body.appendChild(ta);
-        ta.select();
-        document.execCommand('copy');
-        document.body.removeChild(ta);
-        $(this).text("Copied!");
-        var self = $(this);
-        setTimeout(function () {
-            self.text("Copy");
-        }, 3000)
+      var ta = document.createElement('textarea');
+      ta.value = $(this).next()[0].innerText;
+      ta.style.position = 'absolute';
+      ta.style.left = '-3000px';
+      document.body.appendChild(ta);
+      ta.select();
+      document.execCommand('copy');
+      document.body.removeChild(ta);
+      $(this).text("Copied!");
+      var self = $(this);
+      setTimeout(function () {
+          self.text("Copy");
+      }, 3000)
+    }
   }
 
+/**
 $(document).ready(pushdatafromlocation());
 $(document).ready(addcopytoken());
+**/
+
+if(window.addEventListener){
+  window.addEventListener('load', addcopytoken())
+}else{
+  window.attachEvent('onload', addcopytoken())
+}
+
+
+if(window.addEventListener){
+  window.addEventListener('load', pushdatafromlocation())
+}else{
+  window.attachEvent('onload', pushdatafromlocation())
+}
