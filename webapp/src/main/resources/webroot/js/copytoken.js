37953d1c3101b655cb6fc0a92f6b86de3eff6439
Devin S. Olson <devin.olson@hcl.com>
2022-07-06 12:54:27 -0400

    completed code changes for copying the token.

--- webapp/src/main/resources/webroot/js/copytoken.js
+++ webapp/src/main/resources/webroot/js/copytoken.js
@@ -15,36 +15,39 @@
     var btn = document.getElementById('btncopy');
 
     btn.onclick = function (e) {
+      var sp = document.getElementById('refreshToken');
+      var token = sp.innerText;
+
       var ta = document.createElement('textarea');
-      ta.value = $(this).next()[0].innerText;
+      // ta.value = $(this).next()[0].innerText;
+      ta.value = token;
       ta.style.position = 'absolute';
       ta.style.left = '-3000px';
       document.body.appendChild(ta);
       ta.select();
       document.execCommand('copy');
       document.body.removeChild(ta);
-      $(this).text("Copied!");
+
       var self = $(this);
+      self.text("Copied!");
       setTimeout(function () {
           self.text("Copy");
       }, 3000)
     }
+
+    btn.classList.remove('visibility-hidden');
+    btn.classList.remove('display-none');
+
   }
-
-/**
-$(document).ready(pushdatafromlocation());
-$(document).ready(addcopytoken());
-**/
-
-if(window.addEventListener){
-  window.addEventListener('load', addcopytoken())
-}else{
-  window.attachEvent('onload', addcopytoken())
-}
-
 
 if(window.addEventListener){
   window.addEventListener('load', pushdatafromlocation())
 }else{
   window.attachEvent('onload', pushdatafromlocation())
+}
+
+if(window.addEventListener){
+  window.addEventListener('load', addcopytoken())
+}else{
+  window.attachEvent('onload', addcopytoken())
 }
